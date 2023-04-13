27361c5217d1cd7d770f4494ed09f11d5dc45e5b
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-13 15:46:48 +0400

    feat: add social links constants

---
+++ src/constants/socialLinks.ts
@@ -0,0 +1,10 @@
+import { githubPath, linkedInPath, telegramPath } from "./socialLinksPath";
+
+export const SOCIAL_LINKS = [
+  { href: "https://linkedin.com/in/artyomsukiasyan/", path: linkedInPath },
+  { href: "https://t.me/artyom1401", path: telegramPath },
+  {
+    href: "https://github.com/ArtyomSukiasyan",
+    path: githubPath,
+  },
+];
