8d925172348dbf3f57dbd13e3384f8df51083164
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-14 14:06:57 +0400

    feat: update config constants

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,5 @@
-export const TITLE = "Artyom Sukiasyan";
-export const descriptionContent = `Portfolio site about Fullstack JS developer ${TITLE}`;
+export const name = "Artyom";
+export const surname = "Sukiasyan";
+export const fullName = `${name} ${surname}`;
+export const descriptionContent = `Portfolio site about Fullstack JS developer ${fullName}`;
 export const CV_LINK = "/files/cv.pdf";
