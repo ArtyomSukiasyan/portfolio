80479586822b6808a5c4791974fe4e636af1fa57
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-14 14:08:47 +0400

    feat: add preview texts constant

---
+++ src/constants/previewTexts.ts
@@ -0,0 +1,6 @@
+export const skills = "I'm a JavaScript FullStack engineer";
+
+export const description = `I always try to create clean, reusable code that is easy to use. It
+helps to make new features, find and fix bugs faster.`;
+
+export const getInTouch = "Get in touch";
