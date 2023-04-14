e46ebc8f8b9c69cce9963e7f4449403bee1aa8c4
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-14 14:08:05 +0400

    feat: minor changes on page head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import { descriptionContent, TITLE } from "../constants/config";
+import { descriptionContent, fullName } from "../constants/config";
 import Head from "next/head";
 import Header from "../components/header/Header";
+import Preview from "../components/Preview/Preview";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>{TITLE}</title>
+        <title>{fullName}</title>
         <meta name="description" content={descriptionContent} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header />
+      <Preview />
     </>
   );
 }
