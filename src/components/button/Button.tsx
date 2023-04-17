b8642464919ae9a0f32ae3b137657e03962d68cb
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-17 13:38:03 +0400

    feat: update button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,14 @@
+import { IButtonProps } from "../../models/buttonProps";
+
+export default function Button({ href, className, text }: IButtonProps) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      className={`btn ${className}`}
+      rel="noreferrer"
+    >
+      {text}
+    </a>
+  );
+}
