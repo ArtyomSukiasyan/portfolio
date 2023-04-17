d319c58270c9ba3759897127e575b1dbb8c0e770
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-17 13:17:48 +0400

    feat: update social-links component

--- src/components/social-links/SocialLinks.tsx
+++ src/components/social-links/SocialLinks.tsx
@@ -2,9 +2,9 @@ import { SOCIAL_LINKS } from "../../constants/socialLinks";
 import Icon from "../icon/Icon";
 import styles from "./style.module.css";
 
-export default function SocialLinks() {
+export default function SocialLinks({ className = "" }) {
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${className}`}>
       {SOCIAL_LINKS.map((el) => (
         <a href={el.href} target="_blank" key={el.href} rel="noreferrer">
           <Icon path={el.path} />
