68b7eace3c8f255cb4e87e5ad574bca94f492415
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-17 14:04:06 +0400

    feat: add aria-label in links

--- src/components/shared/social-links/SocialLinks.tsx
+++ src/components/shared/social-links/SocialLinks.tsx
@@ -6,7 +6,13 @@ export default function SocialLinks({ className = "" }) {
   return (
     <div className={`${styles.wrapper} ${className}`}>
       {SOCIAL_LINKS.map((el) => (
-        <a href={el.href} target="_blank" key={el.href} rel="noreferrer">
+        <a
+          href={el.href}
+          target="_blank"
+          key={el.name}
+          rel="noreferrer"
+          aria-label={el.name}
+        >
           <Icon path={el.path} />
         </a>
       ))}
--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -3,10 +3,11 @@ import { githubPath, linkedInPath, telegramPath } from "./socialLinksPath";
 export const linkedinLink = "https://linkedin.com/in/artyomsukiasyan/";
 
 export const SOCIAL_LINKS = [
-  { href: linkedinLink, path: linkedInPath },
-  { href: "https://t.me/artyom1401", path: telegramPath },
+  { href: linkedinLink, path: linkedInPath, name: "Linkedin" },
+  { href: "https://t.me/artyom1401", path: telegramPath, name: "Telegram" },
   {
     href: "https://github.com/ArtyomSukiasyan",
     path: githubPath,
+    name: "Github",
   },
 ];
