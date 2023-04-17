1596db58d5b7fd5e3d26e45566e933d3c8b08e71
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-17 23:43:17 +0400

    feat: add education and work experience

---
+++ src/constants/about.tsx
@@ -0,0 +1,27 @@
+import { IAboutList } from "../models/aboutItemProps";
+
+export const EDUCATION: IAboutList[] = [
+  {
+    title: "Armenian Code Academy",
+    role: "JavaScript BootCamp. A 4-month full-time intensive course.",
+    date: "2021",
+  },
+  {
+    title: "Armenian State University of Economics",
+    role: " Master's degree. IT in business",
+    date: "2020-2022",
+  },
+  {
+    title: "Armenian State University of Economics",
+    role: " Bachelor's degree. International economic relations",
+    date: "2014-2020",
+  },
+];
+
+export const WORK: IAboutList[] = [
+  {
+    title: "The Software Development Company",
+    role: "JavaScript engineer",
+    date: "Since 2021",
+  },
+];
