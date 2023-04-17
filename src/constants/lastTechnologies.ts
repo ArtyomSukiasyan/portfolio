0dfc77e5e47637528f89b177255b671599ca63d6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-17 23:29:38 +0400

    feat: add last technologies

---
+++ src/constants/lastTechnologies.ts
@@ -0,0 +1,14 @@
+export const lastTechnologies = [
+  {
+    title: "Basic",
+    skills: ["HTML", "CSS", "JavaScript", "TypeScript", "Git"],
+  },
+  {
+    title: "Frontend",
+    skills: ["ReactJS", "NextJS", "Angular", "SCSS", "Material", "Tauri"],
+  },
+  {
+    title: "Backend",
+    skills: ["Node/Express", "SQL", "MongoDB", "Firebase", "Docker", "Swagger"],
+  },
+];
