82e9536e452b6b26280307ba8bee5254ab3274af
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-17 13:43:58 +0400

    feat: make interface for Img component

--- src/components/shared/img/Img.tsx
+++ src/components/shared/img/Img.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { IImgProps } from "../../../models/imgProps";
 import styles from "./Img.module.css";
 
 export default function Img({
@@ -7,7 +8,7 @@ export default function Img({
   src,
   alt = "",
   className = "",
-}: any) {
+}: IImgProps) {
   return (
     <div className={`${styles.wrapper} ${className}`}>
       <Image width={width} height={height} src={src} alt={alt} />
---
+++ src/models/imgProps.ts
@@ -0,0 +1,7 @@
+export interface IImgProps {
+  width: number;
+  height: number;
+  src: string;
+  alt: string;
+  className: string;
+}
