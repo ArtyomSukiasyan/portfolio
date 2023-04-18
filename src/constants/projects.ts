d0fc8f16453bdc324c92c956bf560aaaaa39801e
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-18 13:20:26 +0400

    feat: add projects constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,13 @@
+export const PROJECTS = [
+  {
+    title: "Chess Arbiter",
+    text: ` 
+      A handbook of chess rules. It can be useful for chess arbiters and chess players, who must know the rules to protect their rights.
+  
+      I created it with my friend. We were both professional chess players. The technical part of the application was made by me.
+      `,
+    image: "/arbiter.png",
+    roles: ["nextJs", "co-founder"],
+    href: "https://chessarbiter.info/",
+  },
+];
