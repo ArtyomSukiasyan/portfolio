7ccff88436298d0f898bf775e084302ea3c7be98
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-19 01:42:44 +0400

    feat: change preview description

--- src/constants/previewTexts.ts
+++ src/constants/previewTexts.ts
@@ -1,6 +1,6 @@
 export const skills = "I'm a JavaScript FullStack engineer";
 
-export const description = `Hard-working web developer with a keen interested in education. 
+export const description = `Hard-working web developer with a keen interested in education. I spend an average of 14 hours a day programming.
 I always try to create clean, reusable code that is easy to use. It helps to make new 
 features, find and fix bugs faster.`;
 
