1af144da309c65687587fcf24964adc85090b58d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-18 13:20:15 +0400

    feat: add project texts

---
+++ src/constants/projectTexts.ts
@@ -0,0 +1,4 @@
+export const visitSite = "Visit Website";
+
+export const internalProjectsText =
+  "Since at my job I write programs for internal use by clients, I can't post them here due to privacy policy. Here is some of my open source work";
