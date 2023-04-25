4ad19a79c865cd4a1cc444dec565cd9f26faeae6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-25 23:57:39 +0400

    feat: add cv

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,5 +2,5 @@ export const name = "Artyom";
 export const surname = "Sukiasyan";
 export const fullName = `${name} ${surname}`;
 export const descriptionContent = `Portfolio site about Fullstack JS developer ${fullName}`;
-export const CV_LINK = "/files/cv.pdf";
+export const CV_LINK = "/files/Artyom_Sukiasyan_JS_Engineer_CV.pdf";
 export const CV_Text = "My CV";
