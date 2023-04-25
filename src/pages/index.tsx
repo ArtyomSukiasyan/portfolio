4c3ab1b2f76b50dfde8521da403ff8e457b9b529
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-04-26 00:42:59 +0400

    feat: add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { descriptionContent, fullName } from "../constants/config";
+import { descriptionContent, fullName, LOGO } from "../constants/config";
 import Head from "next/head";
 import Header from "../components/sections/header/Header";
 import Preview from "../components/sections/preview/Preview";
@@ -13,6 +13,10 @@ export default function Home() {
         <title>{fullName}</title>
         <meta name="description" content={descriptionContent} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${LOGO}</text></svg>`}
+        />
       </Head>
       <Header />
       <Preview />
