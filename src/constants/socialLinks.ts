a6180eb16462db9337191a9b636a4e3242ba1f5a
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-27 20:02:36 +0400

    feat: minor code refactoring

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,14 +1,12 @@
 import { githubPath, linkedInPath, telegramPath } from "./socialLinksPath";
 
 export const linkedinLink = "https://linkedin.com/in/artyomsukiasyan/";
-export const lichessLink = "https://lichess.org/@/artiom1401"
+export const lichessLink = "https://lichess.org/@/artiom1401";
+const telegramLink = "https://t.me/artyom1401";
+const githubLink = "https://github.com/ArtyomSukiasyan";
 
 export const SOCIAL_LINKS = [
   { href: linkedinLink, path: linkedInPath, name: "Linkedin" },
-  { href: "https://t.me/artyom1401", path: telegramPath, name: "Telegram" },
-  {
-    href: "https://github.com/ArtyomSukiasyan",
-    path: githubPath,
-    name: "Github",
-  },
+  { href: telegramLink, path: telegramPath, name: "Telegram" },
+  { href: githubLink, path: githubPath, name: "Github" },
 ];
