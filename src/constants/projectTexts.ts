c07b28a070fc6ff5b39e60cd1d7e6090d07745c7
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-10-29 19:08:07 +0400

    feat: update project text

--- src/constants/projectTexts.ts
+++ src/constants/projectTexts.ts
@@ -1,9 +1,11 @@
 export const visitSite = "Visit Website";
 
+export const sourceCode = "Source code"
+
 export const internalProjectsText =
   "Since the start of job, I wrote government programs and programs for internal use (can't be posted due to NDA). Here is some of my open source projects";
 
 export const githubProjects = "My Github projects";
 
 export const freeTimeText =
-  "In my free time I create various features and publish them in my Github";
+  "In my free time I create various features and publish them in my Github. Below you can see some of my projects.";
