b0b36674d66e51737a5350298281a6d00a96b714
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-05-23 23:30:11 +0400

    feat: change texts

--- src/constants/aboutTexts.ts
+++ src/constants/aboutTexts.ts
@@ -1,5 +1,5 @@
 export const description =
-  "Due to 15+ years of professional chess playing, I have an analytical mind, aimed at the optimal solution of problems. As fan of my work I spend an average of 14 hours a day on programming. I always try to create clean and reusable codes which are easy to use.";
+  "Thanks to extensive experience playing professional chess, I have an analytical mind aimed at optimal problem solving. Being a fan of my craft, I spend an average of 14 hours a day programming. I always try to create clear and reusable codes that are easy to use.";
 
 export const lastTechnologiesText =
   "Here are a few technologies I've been working with recently:";
