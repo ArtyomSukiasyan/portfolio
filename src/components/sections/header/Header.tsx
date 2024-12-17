b8ae357e2079a69e37ce0e4f73a27304caf0594d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-12-17 21:52:44 +0400

    feat: update contacts

--- src/components/sections/header/Header.tsx
+++ src/components/sections/header/Header.tsx
@@ -1,9 +1,8 @@
-import Button from "../../shared/button/Button";
-import SocialLinks from "../../shared/social-links/SocialLinks";
 import Container from "../../shared/container/Container";
 import Img from "../../shared/img/Img";
-import { CV_LINK, CV_Text, fullName } from "../../../constants/config";
+import { fullName } from "../../../constants/config";
 import favicon from "../../../../public/images/favicon.png";
+import Contact from "../contact/Contact";
 import styles from "./Header.module.css";
 
 export default function Header() {
@@ -15,10 +14,7 @@ export default function Header() {
             <Img src={favicon} alt="logo" />
             <p className={styles.fullName}>{fullName}</p>
           </section>
-          <section className={styles.links}>
-            <SocialLinks className={styles.socialLinks} />
-            <Button className={styles.cvButton} href={CV_LINK} text={CV_Text} />
-          </section>
+          <Contact />
         </Container>
       </div>
     </header>
